157c3bdead22813903759e9c669b9d825b14a63e
tejaswiniBoyapati <teju3663@gmail.com>
2021-09-01 16:00:11 +0530

    here yuhh go xD

---
+++ blog/article_script.js
@@ -0,0 +1,184 @@
+       //JavaScript file for articles...
+       
+       //Card 1:
+       function changeColorOver_a()
+        {
+            document.getElementById("card1").style.backgroundColor="black";
+            document.getElementById("card1").style.color="white"
+            
+        }
+        function changeColorOut_a()
+        {
+            document.getElementById("card1").style.backgroundColor="whitesmoke";
+            document.getElementById("card1").style.color="black"
+            
+        }
+
+        //Card 2:
+       function changeColorOver_b()
+       {
+           document.getElementById("card2").style.backgroundColor="black";
+           document.getElementById("card2").style.color="white"
+           
+       }
+       function changeColorOut_b()
+       {
+           document.getElementById("card2").style.backgroundColor="whitesmoke";
+           document.getElementById("card2").style.color="black"
+           
+       }
+
+       //Card 3:
+       function changeColorOver_c()
+        {
+            document.getElementById("card3").style.backgroundColor="black";
+            document.getElementById("card3").style.color="white"
+            
+        }
+        function changeColorOut_c()
+        {
+            document.getElementById("card3").style.backgroundColor="whitesmoke";
+            document.getElementById("card3").style.color="black"
+            
+        }
+
+        //Card 4:
+       function changeColorOver_d()
+       {
+           document.getElementById("card4").style.backgroundColor="black";
+           document.getElementById("card4").style.color="white"
+           
+       }
+       function changeColorOut_d()
+       {
+           document.getElementById("card4").style.backgroundColor="whitesmoke";
+           document.getElementById("card4").style.color="black"
+           
+       }
+
+       //Card 5:
+       function changeColorOver_e()
+        {
+            document.getElementById("card5").style.backgroundColor="black";
+            document.getElementById("card5").style.color="white"
+            
+        }
+        function changeColorOut_e()
+        {
+            document.getElementById("card5").style.backgroundColor="whitesmoke";
+            document.getElementById("card5").style.color="black"
+            
+        }
+
+        //Card 6:
+       function changeColorOver_f()
+       {
+           document.getElementById("card6").style.backgroundColor="black";
+           document.getElementById("card6").style.color="white"
+           
+       }
+       function changeColorOut_f()
+       {
+           document.getElementById("card6").style.backgroundColor="whitesmoke";
+           document.getElementById("card6").style.color="black"
+           
+       }
+
+       //Card 7:
+       function changeColorOver_g()
+        {
+            document.getElementById("card7").style.backgroundColor="black";
+            document.getElementById("card7").style.color="white"
+            
+        }
+        function changeColorOut_g()
+        {
+            document.getElementById("card7").style.backgroundColor="whitesmoke";
+            document.getElementById("card7").style.color="black"
+            
+        }
+
+        //Card 8:
+       function changeColorOver_h()
+       {
+           document.getElementById("card8").style.backgroundColor="black";
+           document.getElementById("card8").style.color="white"
+           
+       }
+       function changeColorOut_h()
+       {
+           document.getElementById("card8").style.backgroundColor="whitesmoke";
+           document.getElementById("card8").style.color="black"
+           
+       }
+
+       //Card 9:
+       function changeColorOver_i()
+        {
+            document.getElementById("card9").style.backgroundColor="black";
+            document.getElementById("card9").style.color="white"
+            
+        }
+        function changeColorOut_i()
+        {
+            document.getElementById("card9").style.backgroundColor="whitesmoke";
+            document.getElementById("card9").style.color="black"
+            
+        }
+
+
+        //ARTICLE LINKS
+        
+        //Article 1
+        function articleA()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/667/amazing-female-photographers-you-should-know")
+        }
+
+        //Article 2
+        function articleB()
+        {
+            window.open("https://www.nyip.edu/photo-articles/business/do-photographers-need-a-college-degree")
+        }
+
+        //Article 3
+        function articleC()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/808/exclusive-interview-with-jacopo-della-valle")
+        }
+
+        //Article 4
+        function articleD()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/953/how-the-art-of-photography-has-influenced-the-world--")
+        }
+
+        //Article 5
+        function articleE()
+        {
+            window.open("https://photographycourse.net/introduction-to-aperture/")
+        }
+
+        //Article 6
+        function articleF()
+        {
+            window.open("https://www.all-things-photography.com/beginning-digital-photography/")
+        }
+
+        //Article 7
+        function articleG()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/805/lckdn20-documenting-the-abandoned-city")
+        }
+
+        //Article 8
+        function articleH()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/976/using-drones-to-strengthen-your-photography-business")
+        }
+
+        //Article 9
+        function articleI()
+        {
+            window.open("https://www.all-about-photo.com/photo-articles/photo-article/930/making-sure-you-stand-out-as-a-freelance-photographer")
+        }
